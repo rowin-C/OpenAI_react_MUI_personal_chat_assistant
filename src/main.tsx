469a85503ff7bf691b9ed2215a182a6584bedba6
Saubhagya Prasad <saubhagyaprasad31122001@gmail.com>
2023-04-30 13:56:21 +0530

    preact

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import "./index.css";
+import { render } from "preact";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+render(<App />, document.getElementById("root") as HTMLElement);
